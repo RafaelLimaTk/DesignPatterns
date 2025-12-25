4559e222dd984fa649be928339275e84a019bfd8
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2025-12-25 16:35:30 -0300

    Adicionar arquivos de projeto.

---
+++ A - Creational/1 - Singleton/Singleton/Implementation.cs
@@ -0,0 +1,30 @@
+﻿namespace Singleton;
+
+/// <summary>
+/// Singleton
+/// </summary>
+public class Logger
+{
+    // Lazy<T>
+    private static readonly Lazy<Logger> _lazyLogger
+        = new(() => new Logger());
+
+    //private static Logger? _instance;
+
+    /// <summary>
+    /// Instance
+    /// </summary>
+    public static Logger Instance => _lazyLogger.Value;
+
+    protected Logger()
+    {
+    }
+
+    /// <summary>
+    /// SingletonOperation
+    /// </summary>
+    public void Log(string message)
+    {
+        Console.WriteLine($"Message to log: {message}");
+    }
+}
