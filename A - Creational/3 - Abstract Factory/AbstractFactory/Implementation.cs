c84a5bf8c4bc638f58f877ccf7248ed408f6f490
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2025-12-25 20:38:52 -0300

    Adiciona exemplo do padrão Abstract Factory em C#
    Inclui novo projeto AbstractFactory com implementação do padrão Abstract Factory, contendo interfaces, produtos e fábricas concretas para Brasil e França, além de classe cliente ShoppingCart e exemplo de uso no Program.cs.

---
+++ A - Creational/3 - Abstract Factory/AbstractFactory/Implementation.cs
@@ -0,0 +1,104 @@
+﻿namespace AbstractFactory;
+
+/// <summary>
+/// Abstract Factory
+/// </summary>
+public interface IShoppingCartPurchaseFactory
+{
+    IDiscountService CreateDiscountService();
+    IShippingCostsService CreateShippingCostsService();
+}
+
+/// <summary>
+/// Abstract Product
+/// </summary>
+public interface IDiscountService
+{
+    int DiscountPercentage { get; }
+}
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class BrazilDiscountService : IDiscountService
+{
+    public int DiscountPercentage => 20;
+}
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class FranceDiscountService : IDiscountService
+{
+    public int DiscountPercentage => 10;
+}
+
+/// <summary>
+/// Abstract Product
+/// </summary>
+public interface IShippingCostsService
+{
+    decimal ShippingCosts { get; }
+}
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class BrazilShippingCostsService : IShippingCostsService
+{
+    public decimal ShippingCosts => 20;
+}
+
+/// <summary>
+/// Concrete Product
+/// </summary>
+public class FranceShippingCostsService : IShippingCostsService
+{
+    public decimal ShippingCosts => 25;
+}
+
+/// <summary>
+/// Concrete Factory
+/// </summary>
+public class BrazilShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+{
+    public IDiscountService CreateDiscountService()
+        => new BrazilDiscountService();
+
+    public IShippingCostsService CreateShippingCostsService()
+        => new BrazilShippingCostsService();
+}
+
+/// <summary>
+/// Concrete Factory
+/// </summary>
+public class FranceShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+{
+    public IDiscountService CreateDiscountService()
+        => new FranceDiscountService();
+
+    public IShippingCostsService CreateShippingCostsService()
+        => new FranceShippingCostsService();
+}
+
+/// <summary>
+/// Client class
+/// </summary>
+public class ShoppingCart
+{
+    private readonly IDiscountService _discountService;
+    private readonly IShippingCostsService _shippingCostsService;
+    private readonly int _orderCosts;
+
+    // ctor
+    public ShoppingCart(IShoppingCartPurchaseFactory factory)
+    {
+        _discountService = factory.CreateDiscountService();
+        _shippingCostsService = factory.CreateShippingCostsService();
+        _orderCosts = 200;
+    }
+
+    public void CalculateCosts()
+        => Console.WriteLine($"Total costs = " +
+            $"{_orderCosts - (_orderCosts / 100 * _discountService.DiscountPercentage) + _shippingCostsService.ShippingCosts}");
+}
---
+++ A - Creational/3 - Abstract Factory/AbstractFactory/Program.cs
@@ -0,0 +1,13 @@
+﻿using AbstractFactory;
+
+Console.Title = "Abstract factory pattern";
+
+var brazilShoppingCartPurchaseFactory = new BrazilShoppingCartPurchaseFactory();
+var shoppingCartForBrazil = new ShoppingCart(brazilShoppingCartPurchaseFactory);
+shoppingCartForBrazil.CalculateCosts();
+
+var franceShoppingCartPurchaseFactory = new FranceShoppingCartPurchaseFactory();
+var shoppingCartForFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+shoppingCartForFrance.CalculateCosts();
+
+Console.ReadKey();
