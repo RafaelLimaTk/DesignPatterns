49cf237053fc3cdf99f74325004dfc6638c4aa1b
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2025-12-29 15:39:08 -0300

    Adiciona implementação do padrão Decorator em C#
    Inclui projeto console para o padrão Decorator com interface IMailService, implementações concretas, decorador base e dois decoradores (estatísticas e armazenamento de mensagens). Adiciona exemplo de uso no Program.cs.

---
+++ B - Structural/8 - Decorator/Decorator/Implementation.cs
@@ -0,0 +1,84 @@
+﻿namespace Decorator;
+
+/// <summary>
+/// Component (As Interface)
+/// </summary>
+public interface IMailService
+{
+    bool SendEmail(string message);
+}
+
+/// <summary>
+/// Concrete Component
+/// </summary>
+public class CloudMailService : IMailService
+{
+    public bool SendEmail(string message)
+    {
+        Console.WriteLine($"Message \"{message}\" " +
+            $"sent via {nameof(CloudMailService)}");
+        return true;
+    }
+}
+
+/// <summary>
+/// Concrete Component
+/// </summary>
+public class OnPremiseMailService : IMailService
+{
+    public bool SendEmail(string message)
+    {
+        Console.WriteLine($"Message \"{message}\" " +
+            $"sent via {nameof(OnPremiseMailService)}");
+        return true;
+    }
+}
+
+/// <summary>
+/// Decorator
+/// </summary>
+public abstract class MailServiceDecoratorBase : IMailService
+{
+    protected readonly IMailService _mailService;
+
+    public MailServiceDecoratorBase(IMailService mailService)
+        => _mailService = mailService;
+
+    public virtual bool SendEmail(string message)
+    {
+        return _mailService.SendEmail(message);
+    }
+}
+
+/// <summary>
+/// Concrete Decorator
+/// </summary>
+public class StatisticsDecorator(IMailService mailService)
+    : MailServiceDecoratorBase(mailService)
+{
+    public override bool SendEmail(string message)
+    {
+        Console.WriteLine($"Collection statistics in: {nameof(StatisticsDecorator)}");
+        return base.SendEmail(message);
+    }
+}
+
+/// <summary>
+/// Concrete Decorator
+/// </summary>
+public class MessageDatabaseDecorator(IMailService mailService)
+    : MailServiceDecoratorBase(mailService)
+{
+    public List<string> SentMessages { get; private set; } = [];
+
+    public override bool SendEmail(string message)
+    {
+        if (base.SendEmail(message))
+        {
+            SentMessages.Add(message);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ B - Structural/8 - Decorator/Decorator/Program.cs
@@ -0,0 +1,28 @@
+﻿using Decorator;
+
+Console.Title = "Decorator pattern";
+
+// implementation mail services
+CloudMailService cloudMailService = new();
+cloudMailService.SendEmail("Hello World!");
+
+OnPremiseMailService onPremiseMailService = new();
+onPremiseMailService.SendEmail("Hello World!");
+
+// add decorated mail services
+var cloudMailServiceWithStats =
+    new StatisticsDecorator(cloudMailService);
+cloudMailServiceWithStats.SendEmail($"Hi there via {nameof(StatisticsDecorator)} wrapper.");
+
+// add decorated mail services
+var cloudMailServiceWithDatabase =
+    new MessageDatabaseDecorator(cloudMailService);
+cloudMailServiceWithDatabase.SendEmail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper 1.");
+cloudMailServiceWithDatabase.SendEmail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper 2.");
+
+foreach (var message in cloudMailServiceWithDatabase.SentMessages)
+{
+    Console.WriteLine($"Storage message: \"{message}\"");
+}
+
+Console.ReadKey();
